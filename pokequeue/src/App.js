dbfa30ce5f0a8bf76b6c0daa62ea356c79e01b9c
msomarri <57019109+msomarri@users.noreply.github.com>
2022-06-10 00:59:51 -0400

    nav bar added

--- pokequeue/src/App.js
+++ pokequeue/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import {Data, Poke, Pokedex}  from "./readData/pokemonInfo"
+import {Data, Poke, Pokedex}  from "./readData/pokemonInfo";
+import {Navbar} from "./Nav/informationBar";
 const  App = () => {
   Data();
   return(
      <div>
      <h1>Welcome to my App</h1>
+     <Navbar />
      </div>
    );
 };
--- pokequeue/src/Nav/informationBar.js
+++ pokequeue/src/Nav/informationBar.js
@@ -2,11 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-export const navBar = () => {
+export const Navbar = () => {
 
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-
-    </nav> 
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">PokeQueue</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pokedex</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Database</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
